028d4911b0af33fe35657957e6b51f71f59d043d
Christos Zoulas <christos@zoulas.com>
2014-12-04 15:22:05 +0000

    Cherry-pick 6bf45271eb8e0e6577b92042ce2003ba998d1686
    Don't bail if there was no error, buf could have been NULL on entry.
    
    Conflicts:
    	src/softmagic.c

--- src/softmagic.c
+++ src/softmagic.c
@@ -32,7 +32,7 @@
 #include "file.h"
 
 #ifndef	lint
-FILE_RCSID("@(#)$File: softmagic.c,v 1.198 2014/11/23 13:54:27 christos Exp $")
+FILE_RCSID("@(#)$File: softmagic.c,v 1.202 2014/11/28 02:46:39 christos Exp $")
 #endif	/* lint */
 
 #include "magic.h"
@@ -1661,7 +1661,7 @@ mget(struct magic_set *ms, const unsigned char *s, struct magic *m,
 			fprintf(stderr, "indirect @offs=%u[%d]\n", offset, rv);
 
 		rbuf = file_pop_buffer(ms, pb);
-		if (rbuf == NULL)
+		if (rbuf == NULL && ms->event_flags & EVENT_HAD_ERR)
 			return -1;
 
 		if (rv == 1) {
